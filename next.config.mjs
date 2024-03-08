52be4461b756a9d1634fe222d2c41a4178655260
Phong Do <dtphong0811@gmail.com>
2024-03-08 12:09:21 +0700

    fix: API_URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: process.env.API_URL || "http://47.129.37.248:3005"
+        API_URL: process.env.API_URL || "https://47.129.37.248:3005"
     }
 };
 
